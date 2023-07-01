127c155a2940a0039be3ba59bce18944921b9b8c
Oleksandr <fedorut20@gmail.com>
2023-07-01 12:32:10 +0300

    Add project files.

---
+++ pass_point_func.cpp
@@ -0,0 +1,13 @@
+// pass_point_func.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
+
+// Subject: Pointers. Passing parameters
+// Task for independent work 2
