461fe1800211dac616da0cc1555b42f8abd1995b
Oleksandr <fedorut20@gmail.com>
2023-07-01 13:35:12 +0300

    save

--- lib.cpp
+++ lib.cpp
@@ -1,2 +1,27 @@
 #include "lib.h"
 
+int* removeNegative(int* arr, int size, int* newSize) {
+
+    int count = 0;
+
+    for (int i = 0; i < size; i++) {
+        if (arr[i] >= 0) {
+            count++;
+        }
+    }
+
+    int* newArr = new int[count];
+
+    int index = 0;
+
+    for (int i = 0; i < size; i++) {
+        if (arr[i] >= 0) {
+            newArr[index] = arr[i];
+            index++;
+        }
+    }
+
+    *newSize = count;
+
+    return newArr;
+}
--- main.cpp
+++ main.cpp
@@ -7,9 +7,30 @@ using namespace std;
 
 int main() {
 
+    int size;
+
+    cout << "Enter the size of the array: ";
+
+    cin >> size;
+
+    int* arr = new int[size];
+
+    cout << "Enter the array elements: ";
+
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "The array elements are: " << endl;
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+
+    delete[] arr;
 
     return 0;
 }
+
 
 
 // Subject: Pointers. Passing parameters
