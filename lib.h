e6e206a539990b3a2ab890c4ff9d469fd87613fd
Oleksandr <fedorut20@gmail.com>
2023-07-01 12:57:20 +0300

    task 1 done

---
+++ lib.h
@@ -0,0 +1,6 @@
+
+
+#include <iostream>
+using namespace std;
+
+void sumAndProduct(int* arr, int size, int* sum, int* mul);
--- main.cpp
+++ main.cpp
@@ -3,11 +3,22 @@
 
 #include <iostream>
 using namespace std;
+#include "lib.h"
 
-int main()
-{
+int main() {
+
+    int arr[] = { 1, 2, 3, 4, 5 };
+
+    int sum, mul;
+  
+    sumAndProduct(arr, 5, &sum, &mul);
+
+    cout << "The sum of the array elements is " << sum << endl;
+    cout << "The product of the array elements is " << mul << endl;
+
     return 0;
 }
+
 
 // Subject: Pointers. Passing parameters
 // Task for independent work 2
